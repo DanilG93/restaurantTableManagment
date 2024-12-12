e001e2b871a142890ad2cce4ac6fb2b0ea40a976
Danil <danilogomanjuk93@gmail.com>
2024-12-12 13:57:54 +0100

    first commit

---
+++ src/main/java/com/softuniProject/RestaurantTableManagment/RestaurantTableManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.softuniProject.RestaurantTableManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantTableManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantTableManagmentApplication.class, args);
+	}
+
+}
